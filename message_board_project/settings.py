9f8bf881346c8002c6551a0a39562b9e18fefe53
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2023-02-01 16:19:09 +0000

    feat: add online database to project

--- message_board_project/settings.py
+++ message_board_project/settings.py
@@ -112,13 +112,24 @@ LOGIN_REDIRECT_URL = "/"
 WSGI_APPLICATION = 'message_board_project.wsgi.application'
 
 
-# Database
+# the Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
